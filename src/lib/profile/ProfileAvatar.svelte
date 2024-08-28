904c5175cecfbb38afd58229f6a328beaf13eaf9
tribesman <mr.dinya@gmail.com>
2024-08-28 17:05:05 +0300

    #5 вывод профиля

---
+++ src/lib/profile/ProfileAvatar.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {baseUrl, apiUrl} from "$lib/store.js";
+
+    export let avatar = "";
+</script>
+
+<div class="w-3/4 mx-auto">
+    {avatar}
+    {#if avatar}
+        <img src="{$apiUrl}/{avatar}" alt="avatar" class="rounded-full aspect-square object-cover"/>
+    {:else}
+        <img src="{$baseUrl}/bleat.jpg" alt="avatar" class="rounded-full aspect-square object-cover"/>
+    {/if}
+</div>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {titleMain, baseUrl, userStore} from '$lib/store.js';
+    import ProfileAvatar from "$lib/profile/ProfileAvatar.svelte";
+</script>
+
+<svelte:head>
+    <title>Профиль - {$titleMain}</title>
+</svelte:head>
+
+{#if $userStore && !$userStore.is_guest}
+    <div>
+        <div class="mb-3">
+            <ProfileAvatar avatar="{($userStore.images && $userStore.images[0]) ? $userStore.images[0] : ''}"/>
+        </div>
+
+        <div class="text-center text-lg mb-3">
+            {$userStore.name ?? 'энписи'}, {$userStore.age}
+        </div>
+    </div>
+
+    <div>
+        <div class="border rounded-md p-3 mb-2 text-center"><a href="{$baseUrl}/profile/data" class="block">Личные
+            данные</a></div>
+        <div class="border rounded-md p-3 mb-2 text-center"><a href="{$baseUrl}/profile/media" class="block">Медиа</a>
+        </div>
+        <div class="border rounded-md p-3 mb-2 text-center"><a href="{$baseUrl}/profile/search" class="block">Настройки
+            поиска</a></div>
+    </div>
+{/if}
