80824ce0b23ca8259da1badef41abcbeb77e227a
tribesman <mr.dinya@gmail.com>
2024-08-29 11:56:49 +0300

    #5 uploadsUrl

--- src/lib/profile/ProfileAvatar.svelte
+++ src/lib/profile/ProfileAvatar.svelte
@@ -1,13 +1,12 @@
 <script>
-    import {baseUrl, apiUrl} from "$lib/store.js";
+    import {baseUrl, uploadsUrl} from "$lib/store.js";
 
     export let avatar = "";
 </script>
 
 <div class="w-3/4 mx-auto">
-    {avatar}
     {#if avatar}
-        <img src="{$apiUrl}/{avatar}" alt="avatar" class="rounded-full aspect-square object-cover"/>
+        <img src="{$uploadsUrl}/{avatar}" alt="avatar" class="rounded-full aspect-square object-cover"/>
     {:else}
         <img src="{$baseUrl}/bleat.jpg" alt="avatar" class="rounded-full aspect-square object-cover"/>
     {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,26 @@
 <script>
     import Header from './Header.svelte';
     import '../app.css';
-    import {baseUrl, apiUrl, titleMain, uuid, darkMode, userStore} from '$lib/store';
+    import {baseUrl, apiUrl, titleMain, uuid, darkMode, userStore, uploadsUrl} from '$lib/store';
     import {AuthGetUUID, AuthGetUserStore} from '$lib/Auth';
     import {onMount} from "svelte";
     import ThemeToggle from "$lib/ThemeToggle.svelte";
+    import {goto} from "$app/navigation";
 
     $darkMode = true;
     $baseUrl = import.meta.env.VITE_ROOT_URL;
     $apiUrl = import.meta.env.VITE_API_URL;
+    $uploadsUrl = $apiUrl;
     $titleMain = "Match Grinder";
 
     onMount(async () => {
         $uuid = await AuthGetUUID();
-        $userStore = await AuthGetUserStore($uuid, `${$apiUrl}/session`);
+        $userStore = await AuthGetUserStore($uuid, `${$apiUrl}/api/v1/user/show`);
+
+        console.log($userStore);
+        if ($userStore.is_guest) {
+            await goto(`${$baseUrl}/signin`);
+        }
     })
 </script>
 
