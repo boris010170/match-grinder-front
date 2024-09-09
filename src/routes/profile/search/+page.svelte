ffff0efa3304f82cfa00c5fe0ae574c18be99b5f
tribesman <mr.dinya@gmail.com>
2024-09-09 15:53:05 +0300

    #6 feature Параметры поиска - переделать параметры на ползунки

--- src/routes/profile/search/+page.svelte
+++ src/routes/profile/search/+page.svelte
@@ -2,6 +2,7 @@
     import {apiUrl, userStore, uuid} from "$lib/store.js";
     import Input from "$lib/Input.svelte";
     import Flash from "$lib/Flash.svelte";
+    import Slider from "$lib/Slider.svelte";
 
     let errorMessage = "";
 
@@ -48,23 +49,29 @@
            options="{[{'value': 'M', 'label': 'Мужской'}, {'value': 'F', 'label': 'Женский'}]}"
            on:change={save}/>
 
-    <Input name="search_radius" label="Радиус поиска" placeholder="Радиус поиск"
-           bind:bindValue="{$userStore.search_radius}" on:change={save}/>
+    <div class="mb-3">
+        Радиус поиска {$userStore.search_radius} км
+        <Slider min="{3}" max="{350}" bind:rangeFrom={$userStore.search_radius}/>
+    </div>
 
-    <Input name="search_from" label="Возраст от" placeholder="Возраст от"
-           bind:bindValue="{$userStore.search_from}" on:change={save}/>
-    <Input name="search_to" label="Возраст до" placeholder="Возраст до" bind:bindValue="{$userStore.search_to}"
-           on:change={save}/>
+    <div class="mb-3">
+        Возраст {$userStore.search_from} - {$userStore.search_to}
+        <Slider min="{18}" max="{60}" bind:rangeFrom={$userStore.search_from} bind:rangeTo={$userStore.search_to}
+                range="{true}"/>
+    </div>
 
-    <Input name="search_height_from" label="Рост от" placeholder="Рост от"
-           bind:bindValue="{$userStore.search_height_from}" on:change={save}/>
-    <Input name="search_height_to" label="Рост до" placeholder="Рост до" bind:bindValue="{$userStore.search_height_to}"
-           on:change={save}/>
+    <div class="mb-3">
+        Рост {$userStore.search_height_from} - {$userStore.search_height_to}
+        <Slider min="{130}" max="{210}" bind:rangeFrom={$userStore.search_height_from}
+                bind:rangeTo={$userStore.search_height_to} range="{true}"/>
+    </div>
 
-    <Input name="search_weight_from" label="Вес от" placeholder="Вес от"
-           bind:bindValue="{$userStore.search_weight_from}" on:change={save}/>
-    <Input name="search_weight_to" label="Вес до" placeholder="Вес до" bind:bindValue="{$userStore.search_weight_to}"
-           on:change={save}/>
+    <div class="mb-3">
+        Вес {$userStore.search_weight_from} - {$userStore.search_weight_to}
+        <Slider min="{40}" max="{150}" bind:rangeFrom={$userStore.search_weight_from}
+                bind:rangeTo={$userStore.search_weight_to} range="{true}"/>
+    </div>
 
 
+    <button class="border rounded-md p-3 mb-2 text-center w-full" on:click={save}>Сохранить</button>
 {/if}
