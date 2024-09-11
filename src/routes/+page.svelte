5c6c5d17c03f87c9e5aa08945330eff2bd29c764
tribesman <mr.dinya@gmail.com>
2024-09-11 09:35:12 +0300

    #9 other главная страница описание от chat gpt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,14 @@
 
 <svelte:head>
     <title>{$titleMain}</title>
+    <meta name="description" content="matchgrinder - tinder который работает в России">
 </svelte:head>
+<div class="text-center mt-36 text-3xl">
+    <p>
+        Matchgrinder предлагает быстрый и удобный поиск пары.</p>
+    <p>Листайте анкеты, находите тех, кто вам
+        понравился, и начните общение только в случае взаимной симпатии.</p>
+    <p>Простой интерфейс и умные алгоритмы помогут вам
+        легко найти идеального партнёра!</p>
+</div>
 
