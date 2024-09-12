5b54a3a63707771de75323f6ba6803569ceac929
tribesman <mr.dinya@gmail.com>
2024-09-12 17:02:13 +0300

    #9 feature вывод фото, следующий профиль

---
+++ src/lib/search/SearchUser.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import {apiUrl} from "$lib/store";
+
+    let imageIndex = 0;
+
+    function nextImage(next = true) {
+
+        if (!user || !user.images) return;
+
+        if (next) {
+            imageIndex++;
+            if (imageIndex >= user.images.length) imageIndex = 0;
+        } else {
+            imageIndex--;
+            if (imageIndex < 0) imageIndex = user.images.length - 1;
+        }
+    }
+
+    export let user: UserStore | undefined = undefined;
+</script>
+{#if user}
+    {#if user.images}
+        <div class="relative">
+            <div class="absolute top-0 w-full h-full z-40">
+                <div class="grid grid-cols-2 gap-5 h-full">
+                    <button class="w-full h-full text-left p-3" on:click={()=>{nextImage(false)}}> ←</button>
+
+                    <button class="w-full h-full text-right p-3 " on:click={()=>{nextImage()}}> →</button>
+                </div>
+            </div>
+
+
+            {#each user.images as image, i}
+                <img src={$apiUrl}/{image.url} alt="{user.name}" class="object-cover w-full aspect-[3/4] opacity-5"
+                     class:hidden="{imageIndex !== i}"/>
+            {/each}
+        </div>
+    {/if}
+
+    <div>
+        {user.name}, {user.age}
+    </div>
+    <div>
+        {user.about}
+    </div>
+{/if}
