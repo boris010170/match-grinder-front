0022229f6e5a37bfabc940709e5a0934df663ac1
tribesman <mr.dinya@gmail.com>
2024-11-07 16:27:17 +0300

    #14 Написать сообщение

--- src/routes/messaging/+page.svelte
+++ src/routes/messaging/+page.svelte
@@ -4,10 +4,11 @@
     import {page} from '$app/stores'
 
     let chat = undefined;
+    let message = "";
+    let sending = false;
     let errorMessage = "";
 
     async function getMessages() {
-
         errorMessage = "";
         try {
             const response = await fetch(`${$apiUrl}/api/v1/chat/view?id=` + $page.url.searchParams.get('id'), {
@@ -28,6 +29,40 @@
             }
         } catch (e) {
             errorMessage = e.message
+        }
+    }
+
+    async function sendMessage(to_id: number) {
+        errorMessage = "";
+        sending = true;
+        try {
+            const response = await fetch(`${$apiUrl}/api/v1/message/create`, {
+                    method: 'POST',
+                    headers: new Headers({
+                        'uuid': $uuid,
+                        'Content-Type': 'application/json',
+                    }),
+                    body: JSON.stringify({
+                        to_id: to_id,
+                        chat_id: $page.url.searchParams.get('id'),
+                        text: message
+                    }),
+                })
+            ;
+
+            if (response.ok) {
+                const newMessage = await response.json();
+                chat.messages = [newMessage, ...chat.messages];
+                console.log(newMessage);
+            } else {
+                let responseMessage = await response.json();
+                errorMessage = responseMessage.message;
+                console.log(errorMessage);
+            }
+            sending = false;
+        } catch (e) {
+            errorMessage = e.message
+            sending = false;
         }
     }
 
@@ -60,5 +95,24 @@
                 </div>
             {/if}
         {/each}
+    </div>
+
+    <div class="flex items-center">
+        <div class="flex-1">
+            <textarea class="border w-full h-20 dark:bg-neutral-800" name="text" bind:value={message}
+                      placeholder="Сообщение..."></textarea>
+        </div>
+        <div class="">
+            <!-- icon666.com - MILLIONS vector ICONS FREE -->
+            <button on:click|preventDefault={async () => await sendMessage(chat.profile.id) }>
+                <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+                     class="fill-neutral-200">
+                    <g id="send">
+                        <path d="m54 32-44 17.59 8.08-17.59-8.08-17.59z"/>
+                    </g>
+                </svg>
+
+            </button>
+        </div>
     </div>
 {/if}
